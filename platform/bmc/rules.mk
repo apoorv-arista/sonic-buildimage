20a272e3429b628e5aa30fd832fe0af867016773
Apoorv Sachan <apoorv@arista.com>
2025-12-17 11:43:32 +0000

    exclude arista platform modules

--- platform/bmc/rules.mk
+++ platform/bmc/rules.mk
@@ -4,8 +4,8 @@
 
 # Include image targets based on the base platform
 ifeq ($(CONFIGURED_PLATFORM),broadcom)
-# Include Arista platform modules for BMC
-include $(PLATFORM_PATH)/platform-modules-arista.mk
+# Include Arista platform modules for BMC (disabled for ARM64)
+#include $(PLATFORM_PATH)/platform-modules-arista.mk
 include $(PLATFORM_PATH)/../bmc/one-image-broadcom.mk
 include $(PLATFORM_PATH)/../bmc/one-aboot-broadcom.mk
 SONIC_ALL += $(SONIC_ONE_IMAGE) $(SONIC_ONE_ABOOT_IMAGE)
